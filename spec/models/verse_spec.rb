1fb45d7af43b12a3c0ff884848a24a9201248e9c
Adrian Checinski <hash4di@gmail.com>
2014-05-26 15:57:59 +0200

    add Verse model spec with validation.

--- spec/models/verse_spec.rb
+++ spec/models/verse_spec.rb
@@ -2,16 +2,71 @@ require 'spec_helper'
 
 describe Verse do
 
-  subject do
-    new_verse = Verse.new :title => 'foo', :description => 'bar'
-    new_verse
-  end
+    let (:new_verse) { build(:verse) }
 
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:label) }
-  it { should_not be_valid }
-  its(:title) { should == 'foo' }
-  its(:description) { should == 'bar' }
+    subject { new_verse }
 
+    context "when valid" do
+      it { should be_valid }
+      specify { new_verse.save.should == true }
+    end
+
+    describe "validate presence of title" do
+      context "when invalid" do
+        let ( :new_verse ) { build( :verse, title: "" ) }
+        it { should_not be_valid }
+        specify { new_verse.save.should == false }
+      end
+
+      context "when valid" do
+        it { should be_valid }
+        specify { new_verse.save.should == true }
+      end
+    end
+
+    describe "validate presence of description" do
+      context "when invalid" do
+        let ( :new_verse ) { build( :verse, description: "" ) }
+        it { should_not be_valid }
+        specify { new_verse.save.should == false }
+      end
+
+      context "when valid" do
+        it { should be_valid }
+        specify { new_verse.save.should == true }
+      end
+    end
+
+    describe "validate presence of label" do
+      context "when invalid" do
+        let ( :new_verse ) { build( :verse, label: "" ) }
+        it { should_not be_valid }
+        specify { new_verse.save.should == false }
+      end
+
+      context "when valid" do
+        it { should be_valid }
+        specify { new_verse.save.should == true }
+      end
+    end
+
+    describe "validate uniquness of title" do
+      it { should validate_uniqueness_of(:title) }
+    end
+
+    describe "validate length of description" do
+      it do
+        should ensure_length_of(:description).
+        is_at_most(120).
+        with_message('Description cannot be more than 120 characters')
+      end
+    end
+
+    describe "validate length of label" do
+      it do
+        should ensure_length_of(:label).
+        is_at_most(20).
+        with_message('Label cannot be more than 20 characters')
+      end
+    end
 end
