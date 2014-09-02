7aa054cef313338177c09f00988168d330c08dec
Adrian Checinski <hash4di@gmail.com>
2014-09-02 15:47:06 +0200

    update view of labels - prevent duplicates.

--- app/helpers/verses_helper.rb
+++ app/helpers/verses_helper.rb
@@ -2,7 +2,7 @@ module VersesHelper
   def label_select(verse)
     select_tag(
       "label_select",
-      options_for_select(current_user.verses.map{|p| [p.label]}, verse.label),
+      options_for_select(current_user.verses.pluck(:label).uniq),
       :class => 'string required',
       :name => 'verse[label]',
       :id => "verse_label",
